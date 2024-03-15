d6783fa985f43aebd93db43fdadc3e5374684d79
Ryan <rom@ualberta.ca>
2024-03-15 05:42:32 -0600

    BraydensCombatScene

--- Game04/Assets/Scripts/Mark.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public enum BookmarkSelected { RED, BLUE }
-public static class Mark 
-{
-    public static BookmarkSelected bookmarkSelected = BookmarkSelected.RED;
-}
